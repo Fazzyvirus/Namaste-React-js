726b327595ba87ebdc3147788fffdf7e4d6cffb8
Fazzyvirus <145501058+Fazzyvirus@users.noreply.github.com>
2024-06-08 18:36:35 +0530

    react

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// const heading = React.createElement("h1", {id:"heading"}, "Hello World From React!");
-// console.log(heading);
-// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
+//JSX - is not HTML in JS although it is HTML or Xml -like syntax
+//JSX (transpile before it reaches JS) - PARCEL - Babel
+// JSX => Babel transpile => React.createElement => ReactElement-JS Object => HTMLElement(render)
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement(
-        "div",
-        {id:"child"},
-        [ React.createElement("h1", {}, "this is Namaste React😁"), React.createElement("h2", {}, "By Akshay Saini")]
-    ),
-    React.createElement(
-        "div",
-        {id:"child2"},
-        [ React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 tag")]
-    )
-)
+const Title = () => (
+  <h1 className="heading" tabIndex="1">
+    Namaste React Title as component using arrow function 🚀
+  </h1>
+);
+
+// const Title = function () {
+//     return (
+//       <h1 className="heading" tabIndex="1">
+//         Namaste React Title as component but using normal function 🚀
+//       </h1>
+//     );
+//   };
+const elem = <span>React Element</span>;
+
+const titles = (
+  <h1 className="heading" tabIndex="1">
+    {elem}
+    Namaste React Title using element 🚀
+  </h1>
+);
+// React Component
+// Class Based Component - OLD
+// Functional Component - NEW
+
+//React Functional Component
+
+// Component Composition
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    {Title()}
+    <Title></Title>
+    <h2>
+      {console.log("you can write js code using these parenthesis anywhere")}
+    </h2>
+    <h1 className="heading">Namaste React Functional Component🚀</h1>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
- 
+root.render(<HeadingComponent />);
