329f7211d0b902e18aae91628700d78a3bcaa9d6
Fazzyvirus <145501058+Fazzyvirus@users.noreply.github.com>
2024-06-28 11:16:27 +0530

    react

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -10,13 +10,13 @@ const RestaurantCard = (props) => {
     deliveryTime,
   } = resData?.info;
   return (
-    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+    <div className="m-4 p-4 w-[230px] rounded-lg bg-gray-200 hover:bg-gray-600 hover:text-white">
       <img
-        className="res-logo"
+        className="rounded-lg"
         src={CDN_URL + cloudinaryImageId}
         alt="res-logo"
       ></img>
-      <h3>{name}</h3>
+      <h3 className="font-bold py-4 text-lg">{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRatingString} stars</h4>
       <h4>{costForTwo}</h4>
